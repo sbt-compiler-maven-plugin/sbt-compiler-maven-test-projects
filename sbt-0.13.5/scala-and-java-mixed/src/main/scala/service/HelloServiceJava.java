8f9398848989a4e88d793357fb65005d8bf4d28d
Grzegorz Slowikowski <gslowikowski@gmail.com>
2014-04-16 06:48:52 +0000

    sbt-compiler-maven-plugin/sbt-compiler-maven-plugin#11 - Create compiler for SBT 0.13.5 (Zinc 0.3.5) - first implementation using Zinc 0.3.5-M3 (SBT 0.13.5-M3).
    git-svn-id: http://sbt-compiler-maven-plugin.googlecode.com/svn/trunk/test-projects@127 d254d13a-b00a-acf6-21f5-a7ca4551998e

---
+++ sbt-0.13.5/scala-and-java-mixed/src/main/scala/service/HelloServiceJava.java
@@ -0,0 +1,12 @@
+package service;
+
+public class HelloServiceJava
+{
+  public static HelloServiceJava instance = new HelloServiceJava();
+
+  public String hello()
+  {
+    return "Hello";
+  }
+
+}
